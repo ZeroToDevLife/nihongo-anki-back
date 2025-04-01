c22513a842f90c45761ad54a6f4f0333337aa569
ZeroToDevLife <cloky930@gmail.com>
2025-04-01 23:20:47 +0900

    docs(main): 메인 애플리케이션 클래스 문서화
    - NihongoApplication 클래스 문서화
    
    - 애플리케이션 시작점 설명 추가

--- src/main/java/com/example/nihongo/NihongoApplication.java
+++ src/main/java/com/example/nihongo/NihongoApplication.java
@@ -3,9 +3,19 @@ package com.example.nihongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * class: 일본어 학습 애플리케이션 메인 클래스
+ * 
+ * description: Spring Boot 애플리케이션의 진입점입니다.
+ */
 @SpringBootApplication
 public class NihongoApplication {
 
+	/**
+	 * description: 애플리케이션 실행 메서드
+	 *
+	 * @param args 명령행 인자
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(NihongoApplication.class, args);
 	}
